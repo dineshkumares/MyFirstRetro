a2655ba913df531aee0f7b9f83173ee93d3e091b
Dinesh Subramanian <dinesh.subramanian@revature.com>
2022-06-08 14:24:11 -0400

    added unit test for viewmodel esp getallrecipe function

--- app/src/test/java/com/example/myfirstretro/BookViewModelTest.kt
+++ app/src/test/java/com/example/myfirstretro/BookViewModelTest.kt
@@ -1,21 +1,86 @@
 package com.example.myfirstretro
 
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.kotlin.Observables
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.kotlin.toObservable
+import io.reactivex.rxjava3.parallel.ParallelFlowable.from
+import io.reactivex.rxjava3.schedulers.Schedulers.from
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnitRunner
 
 
+//@RunWith(MockitoJUnitRunner::class)
 @RunWith(JUnit4::class)
 class BookViewModelTest {
 //    @Mock
 //    @Before
 //    @Test
 //    @After
-//    @Mock
+    lateinit var vm: BookViewModel
+//    lateinit var repo: BookRepository
+    lateinit var recipeList: Observer<List<Recipe>>
 
+//    @Mock
+//    lateinit var dao: RecipeDao
+//
+//    @Mock
+//    lateinit var inter: RetroApiInterface
+
+    @Mock
+    lateinit var repo: BookRepository
+
+    @Before
+    fun setUp(){
+//        MockitoAnnotations.initMocks(this)
+        MockitoAnnotations.openMocks(this)
+//        repo = BookRepository(inter, dao)
+        vm = BookViewModel(repo)
+//        setupObservers()
+    }
+    private fun setupObservers(){
+        recipeList = mock(Observer::class.java) as Observer<List<Recipe>>
+    }
+
+    @Test
+    fun getAllApiRecipeTest(){
+        var fakeList :List<Recipe> = (listOf<Recipe>(
+            Recipe(234,"fromtest","","","","","")
+        ))
+
+        Mockito.`when`(repo.getAllApiRecipe())
+            .thenReturn(Observable.fromArray(fakeList))
+
+
+        val result = vm.getAllApiRecipe()
+
+        result.subscribeBy(
+            onNext = {
+                assertEquals(listOf<Recipe>(
+                    Recipe(234,"fromtest","","","","","")
+                ),it)
+        },
+            onError = { println("error :$it")}
+        )
+
+//        val result = vm.getAllApiRecipe().subscribe(recipeList)
+//
+//        assertEquals(listOf<Recipe>(
+//            Recipe(234,"fromtest","","","","","")
+//        ),result)
+
+//
+    }
 
     @Test
     fun `given repository when calling recipelist then list is empty and assert its empty`() {
