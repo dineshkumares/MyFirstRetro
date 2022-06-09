511f5f38946d24fe9fcafaef19ae0ebdcd2cfc38
Dinesh Subramanian <dinesh.subramanian@revature.com>
2022-06-09 15:08:55 -0400

    Added Timber and also extended it for styling the error as well as handling release Tree, added some comments

--- app/src/main/java/com/example/myfirstretro/MainApp.kt
+++ app/src/main/java/com/example/myfirstretro/MainApp.kt
@@ -2,12 +2,18 @@ package com.example.myfirstretro
 
 import android.app.Application
 import timber.log.Timber
-
+//2 create a class extends Application and add it to manifest
 class MainApp : Application() {
+    //3 ocreate is call implicitly
     override fun onCreate() {
         super.onCreate()
+        //4 check for Buildconfig Debug
         if(BuildConfig.DEBUG) {
+            //5 plant the timber
+//            Timber.plant(Timber.DebugTree())
+
             Timber.plant(object : Timber.DebugTree(){
+                //6 if you want meta data style the log like below
                 override fun createStackElementTag(element: StackTraceElement): String? {
                     return String.format("Class ${super.createStackElementTag(element)} :" +
                             " Line ${element.lineNumber}  Method ${element.methodName}")
@@ -16,7 +22,8 @@ class MainApp : Application() {
             //How your debug messages can be styled or even added more meta data
 
         } else{
-            // build a custom class ReleaseTree and then make it act as an inceptor which will
+            //7 -- if the app is in production
+            // build a custom class ReleaseTree and then make it act as an interceptor which will
             //collect all your logs and puts it to crashlytics
             Timber.plant(ReleaseTree())
         }
--- app/src/main/java/com/example/myfirstretro/ReleaseTree.kt
+++ app/src/main/java/com/example/myfirstretro/ReleaseTree.kt
@@ -3,8 +3,11 @@ package com.example.myfirstretro
 import android.util.Log
 import timber.log.Timber
 
+//8 create a class that extends Timber Tree
 class ReleaseTree: Timber.Tree() {
+    //9 - implement the log function
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        //10 if need only error and warning use the if condition below
 //        if(priority == Log.ERROR || priority == Log.WARN){
 //
 //        }
